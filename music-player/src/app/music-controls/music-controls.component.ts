acef89f252ac864b0a8667bb3883929f2458188b
yeeric01 <57014459+yeeric01@users.noreply.github.com>
2019-11-09 14:10:53 -0600

    Added music-control and browser component (#2)

---
+++ music-player/src/app/music-controls/music-controls.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-music-controls',
+  templateUrl: './music-controls.component.html',
+  styleUrls: ['./music-controls.component.scss']
+})
+export class MusicControlsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
