846c99e009dc303b1ebde687ccb3e3d5b6c601b2
yeeric01 <ericlovesstudying@gmail.com>
2019-11-09 14:41:38 -0600

    added angular material and placeholder play button

--- music-player/src/app/app.module.ts
+++ music-player/src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatButtonModule} from '@angular/material/button';
 import { AppComponent } from './app.component';
 import { MusicControlsComponent } from './music-controls/music-controls.component';
 import { BrowserComponent } from './browser/browser.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes, RouterOutlet } from '@angular/router';
+
+
+const routes: Routes = [
+  { path: 'test', component: MusicControlsComponent }
+];
 
 @NgModule({
   declarations: [
@@ -12,9 +19,18 @@ import { BrowserComponent } from './browser/browser.component';
     BrowserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+
+export class AppModule {
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+  }
+ }
